981156244ef699c319b0889bd6ced2dcf7096145
alvinkibirio-create <alvinkibirio@gmail.com>
2025-09-18 15:27:49 +0300

    Add files via upload

---
+++ assignment .c
@@ -0,0 +1,17 @@
+/*
+Name: Alvin kibirio Mwangi
+Adm no: CT100/G/29153/25
+*/
+#include <stdio.h>
+
+int main() {
+  printf("analogue electronics\n");
+  printf("computer systems and organizaton \n");
+  printf("introduction to programming and alogarithms \n");
+  printf("introduction to computer applications \n");
+  printf("essentials of accounting \n");
+  printf("mathematics of science \n");
+  printf("fundamentals of ict\n");
+  printf("communication and information literacy\n");
+  return 0;
+  }
---
+++ assignment 2 (1).c
@@ -0,0 +1,35 @@
+  //simple C program 
+/*
+Name:Mwangi Alvin kibirio
+Admission number: CT100/G/29153/25
+Description:Showing my profile 
+*/
+#include <stdio.h>//pre-processor direct 
+
+//main function #include <stdio.h>
+
+//main function
+int main(){
+
+    int Height;
+    printf("Enter Your Height: ");
+    scanf("%d", &Height);
+    printf("Your Height is %d\n", Height);
+
+    int Weight;
+    printf("Enter Your Weight: ");
+    scanf("%d", &Weight);
+    printf("Your Weight is %d Kg\n", Weight);
+
+    int contact;
+    printf("Enter Your Contact: ");
+    scanf("%d", &contact);
+    printf("Your contact is %d\n", contact);
+
+    return 0;
+}
+
+
+ 
+ 
+    
